aa21125391e26822617fbb1983921b1cc3aff98b
MilicaJankovic <milica.jankovic787@gmail.com>
2018-02-06 01:00:59 +0100

    .google integration

--- app/src/main/java/com/example/petra/healthylifeapp/Login.java
+++ app/src/main/java/com/example/petra/healthylifeapp/Login.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
@@ -15,13 +16,28 @@ import com.facebook.FacebookSdk;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import java.util.concurrent.Callable;
+
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+
+
 
 public class Login extends AppCompatActivity {
 
     private LoginButton mFacebookSignInButton;
     private CallbackManager mFacebookCallbackManager;
+
+    //google
+    private SignInButton mGoogleSignInButton;
+    private GoogleApiClient mGoogleApiClient;
+
 
     @Override
     protected void onCreate(Bundle  savedInstanceState) {
@@ -62,6 +78,25 @@ public class Login extends AppCompatActivity {
 
         );
 
+        //google
+
+        mGoogleSignInButton = (SignInButton) findViewById(R.id.google_sign_in_button);
+        findViewById(R.id.button_sign_out);
+        mGoogleSignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signInWithGoogle();
+                switch (v.getId()) {
+                    // ...
+                    case R.id.button_sign_out:
+                      signOut();
+                        break;
+
+                }
+            }
+
+        });
+
 
     }
 
@@ -77,5 +112,52 @@ public class Login extends AppCompatActivity {
 
         mFacebookCallbackManager.onActivityResult(requestCode, resultCode, data);
 
+        //google
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+
+            if(result.isSuccess()) {
+                final GoogleApiClient client = mGoogleApiClient;
+                result.getSignInAccount();
+            } else {
+
+
+                //handleSignInResult(...);
+            }
+        } else {
+            // Handle other values for requestCode
+        }
+
     }
+
+    private static final int RC_SIGN_IN = 9001;
+
+    private void signInWithGoogle() {
+        if (mGoogleApiClient != null) {
+            mGoogleApiClient.disconnect();
+        }
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        final Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+    private void signOut() {
+
+        Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(
+                new ResultCallback<Status>() {
+                    @Override
+                    public void onResult(Status status) {
+
+                    }
+                });
+
+
+    }
+
 }
