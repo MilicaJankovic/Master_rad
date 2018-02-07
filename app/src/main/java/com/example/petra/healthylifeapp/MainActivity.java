1685d2188d003fa6b3e225f2caba59815f7103c5
MilicaJankovic <milica.jankovic787@gmail.com>
2018-02-07 23:05:26 +0100

    .google integration fitens API - toast steps and record

--- app/src/main/java/com/example/petra/healthylifeapp/MainActivity.java
+++ app/src/main/java/com/example/petra/healthylifeapp/MainActivity.java
@@ -2,40 +2,157 @@ package com.example.petra.healthylifeapp;
 
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.Scopes;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Scope;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.fitness.Fitness;
+import com.google.android.gms.fitness.FitnessStatusCodes;
+import com.google.android.gms.fitness.data.DataPoint;
+import com.google.android.gms.fitness.data.DataSource;
+import com.google.android.gms.fitness.data.DataType;
+import com.google.android.gms.fitness.data.Field;
+import com.google.android.gms.fitness.data.Subscription;
+import com.google.android.gms.fitness.data.Value;
+import com.google.android.gms.fitness.request.DataSourcesRequest;
+import com.google.android.gms.fitness.request.OnDataPointListener;
+import com.google.android.gms.fitness.request.SensorRequest;
+import com.google.android.gms.fitness.result.DataSourcesResult;
+import com.google.android.gms.fitness.result.ListSubscriptionsResult;
 import com.google.android.gms.location.ActivityRecognition;
 
-public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+import java.util.concurrent.TimeUnit;
+
+public class MainActivity extends AppCompatActivity implements OnDataPointListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
 
     public GoogleApiClient mApiClient;
+
+    private static final int REQUEST_OAUTH = 1;
+    private static final String AUTH_PENDING = "auth_state_pending";
+    private boolean authInProgress = false;
+
+    private Button mCancelSubscriptionsBtn;
+    private Button mShowSubscriptionsBtn;
+
+    private ResultCallback<Status> mSubscribeResultCallback;
+    private ResultCallback<Status> mCancelSubscriptionResultCallback;
+    private ResultCallback<ListSubscriptionsResult> mListSubscriptionsResultCallback;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+//        mApiClient = new GoogleApiClient.Builder(this)
+//                .addApi(ActivityRecognition.API)
+//                .addConnectionCallbacks(this)
+//                .addOnConnectionFailedListener(this)
+//                .build();
+//
+//        mApiClient.connect();
+
+        if (savedInstanceState != null) {
+            authInProgress = savedInstanceState.getBoolean(AUTH_PENDING);
+        }
+
         mApiClient = new GoogleApiClient.Builder(this)
-                .addApi(ActivityRecognition.API)
+                .addApi(Fitness.SENSORS_API)
+                .addApi(Fitness.RECORDING_API)
+                .addScope(new Scope(Scopes.FITNESS_ACTIVITY_READ_WRITE))
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
+                .enableAutoManage(this, 0, this)
                 .build();
 
+        initViews();
+        initCallbacks();
+
+    }
+
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.btn_cancel_subscriptions: {
+                cancelSubscriptions();
+                break;
+            }
+            case R.id.btn_show_subscriptions: {
+                showSubscriptions();
+                break;
+            }
+        }
+    }
+    private void initViews() {
+        mCancelSubscriptionsBtn = (Button) findViewById(R.id.btn_cancel_subscriptions);
+        mShowSubscriptionsBtn = (Button) findViewById(R.id.btn_show_subscriptions);
+
+        mCancelSubscriptionsBtn.setOnClickListener(this);
+        mShowSubscriptionsBtn.setOnClickListener( this);
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
         mApiClient.connect();
     }
-
     @Override
     public void onConnected(@Nullable Bundle bundle) {
-        Intent intent = new Intent(this, ActivityRecognizedService.class);
-        PendingIntent pendingIntent = PendingIntent.getService(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-        ActivityRecognition.ActivityRecognitionApi.requestActivityUpdates(mApiClient, 3000, pendingIntent);
-    }
+//        Intent intent = new Intent(this, ActivityRecognizedService.class);
+//        PendingIntent pendingIntent = PendingIntent.getService(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+//        ActivityRecognition.ActivityRecognitionApi.requestActivityUpdates(mApiClient, 3000, pendingIntent);
 
+        DataSourcesRequest dataSourceRequest = new DataSourcesRequest.Builder()
+                .setDataTypes( DataType.TYPE_STEP_COUNT_CUMULATIVE )
+                .setDataSourceTypes( DataSource.TYPE_RAW )
+                .build();
+
+        ResultCallback<DataSourcesResult> dataSourcesResultCallback = new ResultCallback<DataSourcesResult>() {
+            @Override
+            public void onResult(DataSourcesResult dataSourcesResult) {
+                for( DataSource dataSource : dataSourcesResult.getDataSources() ) {
+                    if( DataType.TYPE_STEP_COUNT_CUMULATIVE.equals( dataSource.getDataType() ) ) {
+                        registerFitnessDataListener(dataSource, DataType.TYPE_STEP_COUNT_CUMULATIVE);
+                    }
+                }
+            }
+        };
+
+        Fitness.SensorsApi.findDataSources(mApiClient, dataSourceRequest)
+                .setResultCallback(dataSourcesResultCallback);
+        Fitness.RecordingApi.subscribe(mApiClient, DataType.TYPE_STEP_COUNT_DELTA)
+                .setResultCallback(mSubscribeResultCallback);
+
+    }
+    private void registerFitnessDataListener(DataSource dataSource, DataType dataType) {
+
+        SensorRequest request = new SensorRequest.Builder()
+                .setDataSource( dataSource )
+                .setDataType( dataType )
+                .setSamplingRate( 3, TimeUnit.SECONDS )
+                .build();
+
+        Fitness.SensorsApi.add( mApiClient, request, this )
+                .setResultCallback(new ResultCallback<Status>() {
+                    @Override
+                    public void onResult(Status status) {
+                        if (status.isSuccess()) {
+                            Log.e( "GoogleFit", "SensorApi successfully added" );
+                        }
+                    }
+                });
+    }
     @Override
     public void onConnectionSuspended(int i) {
 
@@ -43,6 +160,118 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        if( !authInProgress ) {
+            try {
+                authInProgress = true;
+                connectionResult.startResolutionForResult( MainActivity.this, REQUEST_OAUTH );
+            } catch(IntentSender.SendIntentException e ) {
 
+            }
+        } else {
+            Log.e( "GoogleFit", "authInProgress" );
+        }
     }
+
+    @Override
+    public void onDataPoint(DataPoint dataPoint) {
+        for( final Field field : dataPoint.getDataType().getFields() ) {
+            final Value value = dataPoint.getValue( field );
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(getApplicationContext(), "Field: " + field.getName() + " Value: " + value, Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if( requestCode == REQUEST_OAUTH ) {
+            authInProgress = false;
+            if( resultCode == RESULT_OK ) {
+                if( !mApiClient.isConnecting() && !mApiClient.isConnected() ) {
+                    mApiClient.connect();
+                }
+            } else if( resultCode == RESULT_CANCELED ) {
+                Log.e( "GoogleFit", "RESULT_CANCELED" );
+            }
+        } else {
+            Log.e("GoogleFit", "requestCode NOT request_oauth");
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Fitness.SensorsApi.remove( mApiClient, this )
+                .setResultCallback(new ResultCallback<Status>() {
+                    @Override
+                    public void onResult(Status status) {
+                        if (status.isSuccess()) {
+                            mApiClient.disconnect();
+                        }
+                    }
+                });
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBoolean(AUTH_PENDING, authInProgress);
+    }
+
+    private void initCallbacks() {
+        mSubscribeResultCallback = new ResultCallback<Status>() {
+            @Override
+            public void onResult(@NonNull Status status) {
+                if (status.isSuccess()) {
+                    if (status.getStatusCode() == FitnessStatusCodes.SUCCESS_ALREADY_SUBSCRIBED) {
+                        Log.e("RecordingAPI", "Already subscribed to the Recording API");
+                    } else {
+                        Log.e("RecordingAPI", "Subscribed to the Recording API");
+                    }
+                }
+            }
+        };
+
+        mCancelSubscriptionResultCallback = new ResultCallback<Status>() {
+            @Override
+            public void onResult(@NonNull Status status) {
+                if (status.isSuccess()) {
+                    Log.e("RecordingAPI", "Canceled subscriptions!");
+                } else {
+                    // Subscription not removed
+                    Log.e("RecordingAPI", "Failed to cancel subscriptions");
+                }
+            }
+        };
+
+        mListSubscriptionsResultCallback = new ResultCallback<ListSubscriptionsResult>() {
+            @Override
+            public void onResult(@NonNull ListSubscriptionsResult listSubscriptionsResult) {
+                for (Subscription subscription : listSubscriptionsResult.getSubscriptions()) {
+                    DataType dataType = subscription.getDataType();
+                    Log.e("RecordingAPI", dataType.getName());
+                    for (Field field : dataType.getFields()) {
+                        Log.e("RecordingAPI", field.toString());
+                    }
+                }
+            }
+        };
+    }
+
+
+    private void showSubscriptions() {
+        Fitness.RecordingApi.listSubscriptions(mApiClient)
+                .setResultCallback(mListSubscriptionsResultCallback);
+    }
+
+    private void cancelSubscriptions() {
+        Fitness.RecordingApi.unsubscribe(mApiClient, DataType.TYPE_STEP_COUNT_DELTA)
+                .setResultCallback(mCancelSubscriptionResultCallback);
+    }
+
+
 }
